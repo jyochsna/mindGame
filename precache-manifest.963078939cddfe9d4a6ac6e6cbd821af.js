70cc556b33548d7365c097d4e9b436eb7014623b
jyochsna <55848802+jyochsna@users.noreply.github.com>
2020-04-19 21:47:41 -0700

    Updates

---
+++ precache-manifest.963078939cddfe9d4a6ac6e6cbd821af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a147aa9130ebe12226a9f3360e13641",
+    "url": "/mindGame/index.html"
+  },
+  {
+    "revision": "b0ef2652d2c30778fee8",
+    "url": "/mindGame/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "870f45ea82656a5d71e0",
+    "url": "/mindGame/static/css/main.a53caa67.chunk.css"
+  },
+  {
+    "revision": "b0ef2652d2c30778fee8",
+    "url": "/mindGame/static/js/2.10f825c1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mindGame/static/js/2.10f825c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "870f45ea82656a5d71e0",
+    "url": "/mindGame/static/js/main.545c7f3a.chunk.js"
+  },
+  {
+    "revision": "32ab961de36cf8ebcdc9",
+    "url": "/mindGame/static/js/runtime-main.505ca8d8.js"
+  }
+]);
